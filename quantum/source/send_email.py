2d39f140b2c2a49bf7ba5c0a03f1cd264ed0a781
Erick Mas <mas@irides.tohoku.ac.jp>
2022-12-02 18:20:14 +0900

    simulations in mstudio

--- quantum/source/send_email.py
+++ quantum/source/send_email.py
@@ -1,26 +1,3 @@
-import os
-import smtplib
-import params as par
-
-def SendMail(
-    HOST=par.SERVER_HOST,
-    PORT=par.PORT,
-    SENDER=par.EMAIL,
-    PASSWORD=par.EMAIL_PWD,
-    RECIPIENT=par.EMAIL,
-    MESSAGE="Your simulation has finished",
-):
-    server = smtplib.SMTP(host=HOST, port=PORT)
-    server.starttls()
-    server.login(user=SENDER, password=PASSWORD)
-    try:
-        server.sendmail(SENDER, RECIPIENT, MESSAGE)
-        server.quit()
-        print("Successfully sent email")
-    except smtplib.SMTPException:
-        print("Error: unable to send email")
-
-
-if __name__ == "__main__":
-    pwd = os.environ.get(par.EMAIL_PWD)
-    SendMail(PASSWORD=pwd)
+version https://git-lfs.github.com/spec/v1
+oid sha256:5b877fb6377074ec632f17c063ce5608cbaed71f32bf7ea590b9caf1c7dc9b42
+size 646
