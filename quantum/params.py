fd21e9c7cd5280b590d4ec7f9db88113c7a8a400
Erick Mas <mas@irides.tohoku.ac.jp>
2022-12-12 14:57:56 +0900

    outputs

--- quantum/params.py
+++ quantum/params.py
@@ -1,5 +1,5 @@
 # Input parameters
-CASE_NAME = "Zone-2-mssd-c18-15-10"
+CASE_NAME = "Zone-1-20-5"
 
 # Run conditions
 MULTIPLE_RUNS_BOOL = True  # ! requires a list in NUM_SIM_PER_BLOCK
@@ -16,7 +16,7 @@ EMAIL_PWD = "IRIDES_EMAIL_PWD"  # this is the name in the os environment variabl
 
 # Census data source
 CENSUS_FROM_FILE_BOOL = True  # to use the file provided in CENSUS_FILE
-POPULATION_FIELDNAME_IN_FILE = "population" #for MSSD:"population"; for Census:"M_TOTPOP_H"
+POPULATION_FIELDNAME_IN_FILE = "M_TOTPOP_H" #for MSSD:"population"; for Census:"M_TOTPOP_H"
 # If false needs to set below
 BEFORE_2011_BOOL = False  # in case population should be before 3.11
 
@@ -42,20 +42,20 @@ ROAD_NETWORK_FROM_FILE_BOOL = (
 OSM_NTYPE = "drive"  # type of network for OSM
 
 # File locations
-AOS_FILE = "/Volumes/Pegasus32/kochi/zones/zone02/z02_mesh.geojson"
-CENSUS_FILE = "/Volumes/Pegasus32/kochi/zones/zone02/z02_mssd_202203281800.geojson"
-SHELTERS_FILE = "/Volumes/Pegasus32/kochi/zones/zone02/z02_shelters.geojson"
+AOS_FILE = "/Volumes/Pegasus32/kochi/zones/zone01/z01_mesh.geojson"
+CENSUS_FILE = "/Volumes/Pegasus32/kochi/zones/zone01/z01_census.geojson"
+SHELTERS_FILE = "/Volumes/Pegasus32/kochi/zones/zone01/z01_shelters.geojson"
 # MESH_FILE = "/Volumes/Pegasus32/kochi/zones/zone02/z02_mesh.geojson"
 # TOWN_FILE = None
-EDGES_FILE = "/Volumes/Pegasus32/kochi/zones/zone02/z02_edges.geojson"
-NODES_FILE = "/Volumes/Pegasus32/kochi/zones/zone02/z02_nodes.geojson"
+EDGES_FILE = "/Volumes/Pegasus32/kochi/zones/zone01/z01_edges.geojson"
+NODES_FILE = "/Volumes/Pegasus32/kochi/zones/zone01/z01_nodes.geojson"
 
 # Scenario parameters
-SIM_TIME = 15 # in minutes
-MEAN_DEPARTURE = 10  # in minutes
+SIM_TIME = 20 # in minutes
+MEAN_DEPARTURE = 5  # in minutes
 NUM_START = 0  # to start from interemediate point
 NUM_BLOCKS = 1  # to act GLIE in various blocks
-NUM_SIM_PER_BLOCK = [100]  # 5hr, 2d 2h, 20d
+NUM_SIM_PER_BLOCK = [100, 1000, 10000]  # 5hr, 2d 2h, 20d
 
 # Reinforcement Learning parameters
 GLIE_PERCENTAGE = 1.0  # it was on 0.8
